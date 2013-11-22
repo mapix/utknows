4637dec73963b014b893844681de2bbd0a1fd468
luoweifeng <luoweifeng@douban.com>
2013-11-22 12:16:29 +0800

    for upload to pypi

--- setup.py
+++ setup.py
@@ -6,13 +6,15 @@ from setuptools import find_packages, setup
 
 from utknows import __version__
 BASE = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(BASE, 'README.rst')).read()
+README = open(os.path.join(BASE, 'README')).read()
 
 setup(
     name='utknows',
     version=__version__,
     classifiers=['License :: OSI Approved :: BSD License'],
     long_description=README,
+    author="mapix",
+    author_email="mapix.me@gmail.com",
     url='http://mapix.me/utknows/',
     license='BSD',
     packages=['utknows'],
